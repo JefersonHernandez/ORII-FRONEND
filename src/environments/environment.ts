12545838e2bc04845838a713dc85d78d420c3ecf
Jeferson Hernandez <asimplemailmore@gmail.com>
2024-05-29 22:29:16 -0500

    update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  api: 'https://ec2-18-206-172-221.compute-1.amazonaws.com',
+  api: 'http://ec2-18-206-172-221.compute-1.amazonaws.com:3000',
   key: 'DCR15',
   apiCountries: 'names.json',
   apiCodes: 'phone.json',
